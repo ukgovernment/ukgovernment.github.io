fa501396748db75a4b8678e620474182dbf6f56d
ukgovernment <webadmin1000@proton.me>
2023-12-09 14:33:11 +0000

    bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import './assets/main.css'
+// BOOTSTRAP Imports:
+// https://getbootstrap.com/docs/5.3/getting-started/vite/
+import './scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+// import Alert from 'bootstrap/js/dist/alert';
+// import { Tooltip, Toast, Popover } from 'bootstrap';
 
+// Vue Imports
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>This is the home view</h2>
 </template>
