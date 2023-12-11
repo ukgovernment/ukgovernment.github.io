a3f6e9cc3caacf2a20aedeafef067ef335b66d56
ukgovernment <webadmin1000@proton.me>
2023-12-11 14:24:50 +0000

    navbar collapse and href nav

--- src/components/MainNavBar.vue
+++ src/components/MainNavBar.vue
@@ -1,24 +1,25 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-danger border-bottom shadow nav-gradient">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-danger shadow nav-gradient">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <RouterLink to="/" class="nav-link" aria-current="page">Home</RouterLink>
-            <!--<a class="nav-link active" aria-current="page" href="#">Home</a>-->
+            <RouterLink to="/" class="nav-link" aria-current="page">
+                <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                  Home
+                </span>
+            </RouterLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <RouterLink to="/about" class="nav-link" aria-current="page">About</RouterLink>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+              <RouterLink to="/about" class="nav-link" aria-current="page">
+                <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                  About
+                </span>
+              </RouterLink>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
