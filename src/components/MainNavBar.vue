dfc76cc9658602781a5214c68ff9b402d1da2a5b
ukgovernment <webadmin1000@proton.me>
2023-12-12 10:57:12 +0000

    navbar hover and colours

--- src/components/MainNavBar.vue
+++ src/components/MainNavBar.vue
@@ -38,8 +38,29 @@
 </template>
 
 <style scoped>
+
+/* main nav colour and gradient */
 .nav-gradient {
   background: rgb(167,1,1); 
   background: linear-gradient(45deg, rgba(167,1,1,1) 0%, rgba(247,6,4,1) 100%);
 }
+
+/* Dropdown menu background color*/
+.navbar-nav > li > .dropdown-menu {
+  background: rgb(167,1,1); 
+  background: linear-gradient(45deg, rgba(167,1,1,1) 0%, rgba(247,6,4,1) 100%);
+}
+
+/* Dropdown menu item text colour*/
+.dropdown-menu .dropdown-item,
+.dropdown-menu .dropdown-item:link {
+  color: rgba(255, 255, 255, 0.55);
+ }
+ /* Dropdown menu item text colour when hovered over*/
+.dropdown-menu .dropdown-item:hover {
+  color: rgba(255, 255, 255, 0.75);
+  background: rgb(167,1,1); 
+  background: linear-gradient(45deg, rgba(167,1,1,1) 0%, rgba(247,6,4,1) 100%);
+ }
+
 </style>
